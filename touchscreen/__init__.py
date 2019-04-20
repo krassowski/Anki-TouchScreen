3539ea513672d6be542a20638533d77b4f2376e2
krassowski <krassowski.michal@gmail.com>
2019-04-20 19:45:08 +0100

    Version bump

--- touchscreen/__init__.py
+++ touchscreen/__init__.py
@@ -19,7 +19,7 @@ Important parts of Javascript code inspired by http://creativejs.com/tutorials/p
 """
 
 __addon_name__ = "TouchScreen"
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 from aqt import mw, dialogs
 from aqt.utils import showWarning
